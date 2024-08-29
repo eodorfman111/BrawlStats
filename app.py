0611ca8dad7f23a58c7c3b9f7f9d3d2d894bbeff
eodorfman111 <66503717+eodorfman111@users.noreply.github.com>
2024-08-29 15:51:05 -0400

    Update app.py to bind to PORT environment variable

--- app.py
+++ app.py
@@ -85,5 +85,5 @@ def brawler_stats():
         return f"An error occurred: {e}"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
