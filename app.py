514420a893f739dd38924cd41da1732835010e15
eodorfman111 <66503717+eodorfman111@users.noreply.github.com>
2024-09-03 11:30:33 -0400

    Updated app.py to include profile picture and proxy settings

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def stats():
             "most_challenge_wins": player_data.get('bestRoboRumbleTime', 'N/A'),
             "profile_picture_url": player_data['profile_picture_url']
         }
-
+        print("Profile Picture URL:", player_stats['profile_picture_url'])
         stats = calculate_all_stats(battle_log, player_tag)
 
         return render_template('stats.html', stats=stats, player_stats=player_stats)
